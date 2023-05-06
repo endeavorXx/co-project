08eb38715fe22d79ae3777eccb16c19ad8a0f679
Auush121 <132585974+Auush121@users.noreply.github.com>
2023-05-06 16:37:39 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,60 +1,76 @@
 def decimal_to_binary(num):
-    "This function takes integer number ansd returns a string of binary no convertes from decimal"
+    "This function takes integer number and returns a string of binary no converts from decimal"
     binary = ""
-    while num!=0:
-        binary += str(num%2)
-        num = num//2
-    binary = binary[::-1]           #reverses string
+    while num != 0:
+        binary += str(num % 2)
+        num = num // 2
+    binary = binary[::-1]  # reverses string
     return binary
+
+def Find_addr(label,list1):
+    "This function finds the address of a label used for jump statements"
+    ss=""
+    for i in range(len(list1)):
+        if label==list1[i][0]:
+            b=decimal_to_binary(i)
+            for j in range(7-len(b)):
+                ss+="0"
+            ss+=b
+            return ss
+
+
+
 
 def choose_register(reg_num):
     "This function returns opcode of registers"
     return registers[reg_num]
-    
+
+
 def sevenbitbin(abc):
-    a=len(abc)
-    return ("0"*(7-a) + abc)
+    a = len(abc)
+    return ("0" * (7 - a) + abc)
+
 
 opcode = {
-    "add":"00000",
-    "sub":"00001",
-    "mov":["00010","00011"],
-    "ld":"00100",
-    "st":"00101",
-    "mul":"00110",
-    "div":"00111",
-    "rs":"01000",
-    "ls":"01001",
-    "xor":"01010",
-    "or":"01011",
-    "and":"01100",
-    "not":"01101",
-    "cmp":"01110",
-    "jmp":"01111",
-    "jlt":"11100",
-    "jgt":"11101",
-    "je":"11111",
-    "hlt":"11010"
+    "add": "00000",
+    "sub": "00001",
+    "mov": ["00010", "00011"],
+    "ld": "00100",
+    "st": "00101",
+    "mul": "00110",
+    "div": "00111",
+    "rs": "01000",
+    "ls": "01001",
+    "xor": "01010",
+    "or": "01011",
+    "and": "01100",
+    "not": "01101",
+    "cmp": "01110",
+    "jmp": "01111",
+    "jlt": "11100",
+    "jgt": "11101",
+    "je": "11111",
+    "hlt": "11010"
 }
 
-
-
 registers = {
-    "R0":"000",
-    "R1":"001",
-    "R2":"010",
-    "R3":"011",
-    "R4":"100",
-    "R5":"101",
-    "R6":"110",
-    "FLAGS":"111"
-             }
-
-
+    "R0": "000",
+    "R1": "001",
+    "R2": "010",
+    "R3": "011",
+    "R4": "100",
+    "R5": "101",
+    "R6": "110",
+    "FLAGS": "111"
+}
 
 f = open("input.txt")
-w = open("output.txt","a")
+w = open("output.txt", "a")
 nlines = len(f.readlines())
+f.seek(0)
+
+sentence_list=f.read().split("\n")
+List_of_words=[line.split() for line in sentence_list]
 f.seek(0)
 
 for i in range(nlines):
@@ -62,83 +78,118 @@ for i in range(nlines):
     code = ""
     print(query)
     # Type A instruction
-    if query[0]=="add":
+    if query[0] == "add":
         code += opcode["add"]
         code += "00"
         code += choose_register(query[1])
         code += choose_register(query[2])
-        code += choose_register(query[3]) +"\n"
+        code += choose_register(query[3]) + "\n"
         w.write(code)
 
 
-    elif query[0]=="sub":
+    elif query[0] == "sub":
         code += opcode["sub"]
         code += "00"
         code += choose_register(query[1])
         code += choose_register(query[2])
-        code += choose_register(query[3]) +"\n"
+        code += choose_register(query[3]) + "\n"
         w.write(code)
 
 
-    elif query[0]=="mul":
+    elif query[0] == "mul":
         code += opcode["mul"]
         code += "00"
         code += choose_register(query[1])
         code += choose_register(query[2])
-        code += choose_register(query[3]) +"\n"
+        code += choose_register(query[3]) + "\n"
         w.write(code)
-   
 
-    elif query[0]=="xor":
+
+    elif query[0] == "xor":
         code += opcode["xor"]
         code += "00"
         code += choose_register(query[1])
         code += choose_register(query[2])
-        code += choose_register(query[3]) +"\n"
+        code += choose_register(query[3]) + "\n"
         w.write(code)
- 
 
-    elif query[0]=="or":
+
+    elif query[0] == "or":
         code += opcode["or"]
         code += "00"
         code += choose_register(query[1])
         code += choose_register(query[2])
-        code += choose_register(query[3]) +"\n"
+        code += choose_register(query[3]) + "\n"
         w.write(code)
-   
 
-    elif query[0]=="and":
+
+    elif query[0] == "and":
         code += opcode["and"]
         code += "00"
         code += choose_register(query[1])
         code += choose_register(query[2])
-        code += choose_register(query[3]) +"\n"
+        code += choose_register(query[3]) + "\n"
         w.write(code)
-   
+
 
     # Type B instruction
-    elif ((query[0] == "mov") & (query[2][0]=="$")):  
-        code+=opcode["mov"][0]
+    elif ((query[0] == "mov") & (query[2][0] == "$")):
+        code += opcode["mov"][0]
         code += "0"
-        code+=choose_register(query[1])
+        code += choose_register(query[1])
         num = int(query[2][1:])
-        code+=sevenbitbin(decimal_to_binary(num)) +"\n"
+        code += sevenbitbin(decimal_to_binary(num)) + "\n"
         w.write(code)
+
     elif query[0] == "rs":
-        code+=opcode["rs"]
+        code += opcode["rs"]
         code += "0"
         code += choose_register(query[1])
         num = int(query[2][1:])
         code += sevenbitbin(decimal_to_binary(num)) + "\n"
         w.write(code)
+
     elif query[0] == "ls":
-        code+=opcode["ls"] 
+        code += opcode["ls"]
         code += "0"
         code += choose_register(query[1])
         num = int(query[2][1:])
         code += sevenbitbin(decimal_to_binary(num)) + "\n"
         w.write(code)
-#Type C instruction 
-    elif((len(query)==3) & (query[0]=="mov" or query[0]=="div" or query[0]=="not" or query[0]=="cmp") & (query[2] in registers)):
-        code+=opcode[query[0]][1] + "00000" + registers[query[1]] + registers[query[2]] +"\n"
+
+    # Type C instruction
+    elif ((len(query) == 3) & (query[0] == "mov" or query[0] == "div" or query[0] == "not" or query[0] == "cmp") & (
+            query[2] in registers)):
+        code += opcode[query[0]][1] + "00000" + registers[query[1]] + registers[query[2]] + "\n"
         w.write(code)
+
+    #Type E instruction
+    
+    elif query[0]=="jmp":
+        code +=opcode["jmp"]
+        code +="0000"
+        code +=Find_addr(query[1],List_of_words)
+        w.write(code)
+
+    elif query[0]=="jlt":
+        code +=opcode["jlt"]
+        code +="0000"
+        code += Find_addr(query[1], List_of_words)
+        w.write(code)
+
+    elif query[0]=="jgt":
+        code += opcode["jgt"]
+        code += "0000"
+        code += Find_addr(query[1], List_of_words)
+        w.write(code)
+
+    elif query[0]=="je":
+        code += opcode["je"]
+        code += "0000"
+        code += Find_addr(query[1], List_of_words)
+        w.write(code)
+
+
+
+
+
